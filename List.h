8dcddf6cd3406765641874ea37ddff29660fa338
Noah <noaheasley408@gmail.com>
2021-02-05 11:56:51 -0600

    added additional functions for list

--- List.h
+++ List.h
@@ -14,4 +14,18 @@ public:
 	Iterator<T> end();
 	bool contains(const T& object);
 	const void pushFront(const T& value);
+	void pushBack(const T& value);
+	bool insert(const T& value, int index);
+	bool remove(const T& value);
+	const void print();
+	void initalize();
+	const bool isEmpty();
+	bool getData(iterator<T>& iter, int index);
+	const int getLength();
+	const List<T>& operator = (const List<T>& otherList);
+	void sort();
+private:
+	Node<T> m_head;
+	Node<T> m_tail;
+	int m_nodeCount;
 };
