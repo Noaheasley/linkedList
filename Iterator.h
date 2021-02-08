8c8928e5b5b260afa5d506912cb96c2701cbc74f
Noah <noaheasley408@gmail.com>
2021-02-08 16:57:14 -0600

    added definitions for node and iterations

--- Iterator.h
+++ Iterator.h
@@ -5,12 +5,35 @@ template<typename T>
 class Iterator
 {
 public:
-	Iterator<T>();
+	Iterator<T>() { };
 	Iterator<T>(Node<T>* node);
 	Iterator<T> operator ++();
 	Iterator<T> operator --();
 	const bool Iterator<T> operator ==(const Iterator<T>& iter);
 	const bool operator != (const Iterator<T>& iter);
 	T operator* ();
-	Node<T> current;
+private:
+	Node<T> m_current;
 };
+
+template<typename T>
+inline Iterator<T>::Iterator(Node<T>* node)
+{
+	m_current = node;
+}
+
+template<typename T>
+inline Iterator<T> Iterator<T>::operator++()
+{
+	if (current.next)
+		current = current.next;
+	return Iterator<T>(current);
+}
+
+template<typename T>
+inline Iterator<T> Iterator<T>::operator--()
+{
+	if (current.previous)
+		current = current.previous;
+	return Iterator<T>(current);
+}
--- Node.h
+++ Node.h
@@ -6,10 +6,16 @@ template<typename T>
 class Node
 {
 public:
-	Node<T>();
+	Node<T>() { };
 	Node<T>(T value);
 
 	Node<T>* next;
 	Node<T>* previous;
 	T data;
 };
+
+template<typename T>
+inline Node<T>::Node(T value)
+{
+	data = value;
+}
